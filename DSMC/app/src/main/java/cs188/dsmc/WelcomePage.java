28af020ec9beb576590fb659a25b864ffac55ce0
mbmcallister <michael.mcallister@drake.edu>
2016-09-22 16:35:12 -0500

    Intent on Welcome Page
    Made Intent to access Homepage

--- DSMC/app/src/main/java/cs188/dsmc/WelcomePage.java
+++ DSMC/app/src/main/java/cs188/dsmc/WelcomePage.java
@@ -1,13 +1,26 @@
 package cs188.dsmc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomePage extends Activity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomePage.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
