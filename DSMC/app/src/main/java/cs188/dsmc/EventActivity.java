4fe3f1d68779d7c5fc7a907729eba3d6a86573a2
mbmcallister <michael.mcallister@drake.edu>
2016-09-22 14:03:10 -0500

    EventActivity Fixes
    Fixed ImageButton Functionality issues, began work on adding to
    calendar features.

--- DSMC/app/src/main/java/cs188/dsmc/EventActivity.java
+++ DSMC/app/src/main/java/cs188/dsmc/EventActivity.java
@@ -1,7 +1,9 @@
 package cs188.dsmc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -15,16 +17,18 @@ public class EventActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        // image = (ImageButton) findViewById(R.id.imageView);
+        image = (ImageButton) findViewById(R.id.imageButton);
         eventText = (TextView) findViewById(R.id.eventText);
         dateText = (TextView) findViewById(R.id.dateText);
         eventDescription = (TextView) findViewById(R.id.eventDescription);
-       /* image.setOnClickListener(new View.OnClickListener(){
+        image.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setData(CalendarContract.Events.CONTENT_URI);
+                startActivity(calIntent);
             }
-        });*/
+        });
     }
 }
 
