2631f9be1cca8396fd2fec794cd0e74cac7648a4
mbmcallister <michael.mcallister@drake.edu>
2016-09-24 18:45:29 -0500

    messing around with add button

--- DSMC/app/src/main/java/cs188/dsmc/EventActivity2.java
+++ DSMC/app/src/main/java/cs188/dsmc/EventActivity2.java
@@ -3,6 +3,7 @@ package cs188.dsmc;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,10 +18,19 @@ public class EventActivity2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event2);
-        // image = (ImageButton) findViewById(R.id.imageView);
+        image = (ImageButton) findViewById(R.id.imageButton);
         eventText = (TextView) findViewById(R.id.eventText);
         dateText = (TextView) findViewById(R.id.dateText);
         eventDescription = (TextView) findViewById(R.id.eventDescription);
         eventDescription = (TextView) findViewById(R.id.eventDescription);
+        image.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setData(CalendarContract.Events.CONTENT_URI);
+                startActivity(calIntent);
+            }
+        });
     }
+
 }
