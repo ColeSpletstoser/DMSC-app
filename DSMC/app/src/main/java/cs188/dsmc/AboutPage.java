cff678f47c58c92d8a45e153b265d3d71c06c4a6
ColeSpletstoser <cole.spletstoser@drake.edu>
2016-09-25 17:49:44 -0500

    Settings page
    Made change categories clickable. About page not working for some reason

--- DSMC/app/src/main/java/cs188/dsmc/AboutPage.java
+++ DSMC/app/src/main/java/cs188/dsmc/AboutPage.java
@@ -1,9 +1,19 @@
 package cs188.dsmc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by earleyneverlate on 9/25/16.
  */
 public class AboutPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_page);
+    }
+
 }
--- DSMC/app/src/main/java/cs188/dsmc/SettingsPage.java
+++ DSMC/app/src/main/java/cs188/dsmc/SettingsPage.java
@@ -4,20 +4,42 @@ package cs188.dsmc;
  * Created by earleyneverlate on 9/21/16.
  */
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SettingsPage extends Activity {
+    private TextView catTextView;
+    private TextView aboutTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_pahe);
 
+        catTextView = (TextView) findViewById(R.id.textView3);
+        aboutTextView = (TextView) findViewById(R.id.textView7);
         Switch mySwitch = (Switch)findViewById(R.id.mySwitch);
+
+        catTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsPage.this, WelcomePage.class);
+                startActivity(intent);
+            }
+        });
+
+        aboutTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsPage.this, AboutPage.class);
+                startActivity(intent);
+            }
+        });
 
         mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
